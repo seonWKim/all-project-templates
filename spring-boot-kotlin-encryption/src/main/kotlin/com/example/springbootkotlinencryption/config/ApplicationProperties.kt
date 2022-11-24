a407799341c3835b37e8b7b9d8fff91fc23841ca
seonwoo960000 <seonwoo960000.kim@naver.com>
2022-11-25 06:59:16 +0900

    Temp

--- spring-boot-kotlin-encryption/src/main/kotlin/com/example/springbootkotlinencryption/config/ApplicationProperties.kt
+++ spring-boot-kotlin-encryption/src/main/kotlin/com/example/springbootkotlinencryption/config/ApplicationProperties.kt
@@ -2,7 +2,6 @@ package com.example.springbootkotlinencryption.config
 
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
-import org.springframework.context.annotation.Configuration
 
 @ConstructorBinding
 @ConfigurationProperties(prefix = "application")
