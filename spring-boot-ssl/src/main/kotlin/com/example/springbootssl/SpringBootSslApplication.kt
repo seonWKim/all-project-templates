75f193fc5cb9a55c94a8802861a5d80afe5a194e
seonwoo960000 <seonwoo960000.kim@naver.com>
2022-11-25 09:22:14 +0900

    set up project

---
+++ spring-boot-ssl/src/main/kotlin/com/example/springbootssl/SpringBootSslApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springbootssl
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootSslApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBootSslApplication>(*args)
+}
---
+++ spring-boot-ssl/src/test/kotlin/com/example/springbootssl/SpringBootSslApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springbootssl
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootSslApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
