232c1bb869cb4b0b6bde1880c2099f0c9ff35d87
seonwoo960000 <seonwoo960000.kim@naver.com>
2022-11-28 08:20:20 +0900

    initial commit

---
+++ spring-batch-kotlin/src/main/kotlin/com/example/springbatchkotlin/SpringBatchKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springbatchkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBatchKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBatchKotlinApplication>(*args)
+}
