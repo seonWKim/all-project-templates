83f76e8470f6481e60fd6c6f44cb78865b8d22f1
Kim Seon Woo <seonwoo960000@naver.com>
2023-12-03 14:38:11 +0900

    Add simplified ddd project

---
+++ spring-boot-ddd-simplified/src/main/kotlin/com/example/springbootdddsimplified/SpringBootDddSimplifiedApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springbootdddsimplified
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootDddSimplifiedApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBootDddSimplifiedApplication>(*args)
+}
---
+++ spring-boot-ddd-simplified/src/test/kotlin/com/example/springbootdddsimplified/SpringBootDddSimplifiedApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springbootdddsimplified
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootDddSimplifiedApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
